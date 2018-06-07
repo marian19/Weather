3f4d6760ee3d9bf172768be69fcd823f75135527
Marian <marian@Marians-MacBook-Pro.local>
2018-06-08 00:10:27 +0200

    initial commit with pods

---
+++ Weather/Weather/Data Layer/HttpClient/APIClient.swift
@@ -0,0 +1,15 @@
+//
+//  APIClient.swift
+//  Blends
+//
+//  Created by Marian on 12/21/17.
+//  Copyright © 2017 Marian. All rights reserved.
+//
+
+import Foundation
+
+protocol APIClient {
+    func getRequest(path: String, parameters: [String: Any]?, completion:@escaping (Data?, Error?) -> Void)
+    func postRequest(path: String, parameters: [String: Any]?, completion:@escaping (Data?, Error?) -> Void)
+    func putRequest(path: String, parameters: [String: Any]?, completion:@escaping (Data?, Error?) -> Void)
+}
